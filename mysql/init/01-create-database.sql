ac68f85b02f32adefd93caf088acc6d97165c66b
yunrry <yunryy329@gmail.com>
2025-09-01 16:02:43 +0900

    reverse gitignore

---
+++ mysql/init/01-create-database.sql
@@ -0,0 +1,2 @@
+-- Create flik_db database and user
+CREATE DATABASE IF NOT EXISTS flik_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
---
+++ src/test/java/yunrry/flik/FlikApplicationTests.java
@@ -0,0 +1,15 @@
+package yunrry.flik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class FlikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
