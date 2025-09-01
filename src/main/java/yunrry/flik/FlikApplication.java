bf4071bac4e9a9df11e9f1d18152baab89602941
yunrry <yunryy329@gmail.com>
2025-09-01 11:58:11 +0900

    init

---
+++ src/main/java/yunrry/flik/FlikApplication.java
@@ -0,0 +1,13 @@
+package yunrry.flik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlikApplication.class, args);
+    }
+
+}
---
+++ src/test/java/yunrry/flik/FlikApplicationTests.java
@@ -0,0 +1,13 @@
+package yunrry.flik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
